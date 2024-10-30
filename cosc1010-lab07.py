a10888cb02a8e78fd10b4c021e84c6ccef989f8f
DeaconSteiner <deaconsteiner.it@gmail.com>
2024-10-29 18:26:15 -0600

    Added error checking and continuation after calculation

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -81,44 +81,54 @@ print("*"*75)
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
 
-equation = input("Please enter an expression to be calculated: ")
-equation = equation.replace(" ", "")
+while True:
+    equation = input("Please enter an expression to be calculated (or type exit to quit): ")
+    equation = equation.replace(" ", "")
 
-operators = ['+', '-', '*', '/']
+    if equation == "exit".lower():
+        print("Program has been exited")
+        break
+    
+    else:
 
-for char in equation:
-    if char in operators:
-        operator = char
+        operators = ['+', '-', '*', '/']
+
+        for char in equation:
+            if char in operators:
+                operator = char
+            
+        eqn_splt = equation.partition(operator)
         
-eqn_splt = equation.partition(operator)
+        if eqn_splt[0].isdigit() and eqn_splt[-1].isdigit():
+            operand_1 = float(eqn_splt[0])
+            operand_2 = float(eqn_splt[-1])
+            
+        else:
+            print("error occured, try again")
+            continue
 
-operand_1 = float(eqn_splt[0])
-operand_2 = float(eqn_splt[-1])
+        class calculator:
+
+            def add(a, b):
+                return a + b
+            def subtract(a, b):
+                return a - b
+            def multiply(a, b):
+                return a * b
+            def divide(a, b):
+                return a / b
         
+        answer = ''
+        if operator == '+':
+            answer = calculator.add(operand_1, operand_2)
+            
+        elif operator == '-':
+            answer = calculator.subtract(operand_1, operand_2)
+            
+        elif operator == '*':
+            answer = calculator.multiply(operand_1, operand_2)
+            
+        elif operator == '/':
+            answer = calculator.divide(operand_1, operand_2)
 
-class calculator:
-
-    def add(a, b):
-        return a + b
-    def subtract(a, b):
-        return a - b
-    def multiply(a, b):
-        return a * b
-    def divide(a, b):
-        return a / b
-    
-answer = ''
-if operator == '+':
-    answer = calculator.add(operand_1, operand_2)
-    
-elif operator == '-':
-    answer = calculator.subtract(operand_1, operand_2)
-    
-elif operator == '*':
-    answer = calculator.multiply(operand_1, operand_2)
-    
-elif operator == '/':
-    answer = calculator.divide(operand_1, operand_2)
-
-print(answer)
-
+        print(answer)
